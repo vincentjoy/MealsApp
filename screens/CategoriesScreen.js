9cf944009da51205b81486c1d704b5832ea7cb6f
Vincent Joy <vincentjoyhere@gmail.com>
2025-06-06 14:35:24 +0530

    finished the grid styling

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -12,6 +12,7 @@ function CategoriesScreen() {
             data={CATEGORIES}
             keyExtractor={(item) => item.id}
             renderItem={renderCategoryItem}
+            numColumns={2}
         />
     );
 }
