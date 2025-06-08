9191f91ad1b6c5d8a80dc880b917ca0fddb7207f
Vincent Joy <vincentjoyhere@gmail.com>
2025-06-08 21:31:27 +0530

    added an icon on navigator

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,11 +1,31 @@
+import { useLayoutEffect } from "react";
 import { Text, Image, View, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
+import IconButton from '../components/IconButton';
 import List from "../components/MealDetail/List";
-function MealDetailsScreen({ route }) {
+function MealDetailsScreen({ route, navigation }) {
     const mealId = route.params.mealId
     const selectedMeal = MEALS.find((meal) => meal.id === mealId)
+
+    function headerButtonPressHandler() {
+        console.log('Pressed!');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return (
+                    <IconButton
+                        icon="star"
+                        color="white"
+                        onPress={headerButtonPressHandler}
+                    />
+                );
+            },
+        });
+    }, [navigation, headerButtonPressHandler]);
 
     return (
         <ScrollView style={styles.rootContainer}>
